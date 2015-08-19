dd7fb8ddf995ca591726da2bf91c128805d1fa91
Chloe Graham <chloe.graham10@gmail.com>
2015-08-19 15:09:21 +1200

    select tile to move to

--- Cluedo/src/view/BoardCanvas.java
+++ Cluedo/src/view/BoardCanvas.java
@@ -4,6 +4,9 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -17,12 +20,13 @@ import model.Board;
  *
  */
 @SuppressWarnings("serial")
-public class BoardCanvas extends JPanel {
+public class BoardCanvas extends JPanel implements MouseMotionListener, MouseListener{
 	private Board board;
 	
 	public BoardCanvas(Board b){
 		super();
-		
+		this.addMouseMotionListener(this);
+		this.addMouseListener(this);
 		this.board = b;
 		
 		this.setPreferredSize(new Dimension(b.getWidth()*Board.tileSize, b.getHeight()*Board.tileSize));
@@ -46,12 +50,57 @@ public class BoardCanvas extends JPanel {
 		java.net.URL imageURL = BoardCanvas.class.getClassLoader().getResource(filename);
 		
 		try {
-			Image img = ImageIO.read(imageURL);
+			Image img = ImageIO.read(imageURL); 
 			return img;
 		} catch (IOException e) {
 			// we've encountered an error loading the image. There's not much we
 			// can actually do at this point, except to abort the game.
 			throw new RuntimeException("Unable to load image: " + filename);
 		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// user tying to move player 
+		//double mouseX = (int) /tileSize to get coordinate of tile
+		double mouseX = e.getX() / board.tileSize;
+		double mouseY = e.getY() / board.tileSize;
+		selectedTile = board.getTile(mouseX, mouseY);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
